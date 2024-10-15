c4951587b55d7d3c07565864595278005774cc72
Flavia <139448717+flaviaros4@users.noreply.github.com>
2024-10-15 20:47:06 -0300

    update

---
+++ script.js
@@ -0,0 +1,31 @@
+document
+    .getElementById("inscricaoform")
+    .addEventListener("submit", function (event) {
+        event.preventDefault();
+        const formData = {
+            nome: document.getElementById("nome").value,
+            email: document.getElementById("email").value,
+            genero: document.querySelector('input[name="genero"]:checked').value,
+            cidade: document.getElementById("cidade").value,
+            estado: document.getElementById("estado").value,
+            notificacoes: document.querySelector('input[name="notificacoes"]').checked,
+        };
+        console.log("Dados:", formData);
+
+        fetch("https://flaviaros4.github.io/trabalho-client-side", {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(formData),
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log("Success:", data);
+                alert("Inscrição realizada com sucesso!");
+            })
+            .catch((error) => {
+                console.error("Error:", error);
+                alert("Ocorreu um erro ao realizar a inscrição.");
+            });
+    });
